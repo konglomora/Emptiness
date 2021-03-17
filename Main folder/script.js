da95a8745b0d161352940514c8a7e209a23d43f8
Arsenii <arsenii.netrebenko@gmail.com>
2021-03-17 03:01:25 +0200

    👌 IMPROVE: Previous file system and names

---
+++ Main folder/script.js
@@ -0,0 +1,13 @@
+$(function(){
+	$('#scroll_to_crew').click(function(){
+		$('html, body').animate({scrollTop: $(document).height() - $(window).height()}, 600);
+		return false;
+	});
+});
+
+(function(){
+	('#scroll_to_values').click(function(){
+		('html, body').animate({scrollTop: $(document).height() - $(window).height()}, 600);
+		return false;
+	});
+});
